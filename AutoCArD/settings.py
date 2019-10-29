5dfa1cdf55ae25e955dae7d63001284885a9af2d
perext5528 <perext5528@gmail.com>
2019-10-29 22:28:02 +0900

    Changed Installed apps, STATIC_ROOT

--- AutoCArD/settings.py
+++ AutoCArD/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
